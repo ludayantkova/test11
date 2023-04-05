285a13b899805a28563711422932c23085fea0bb
liuda yantkova <127305741+ludayantkova@users.noreply.github.com>
2023-04-05 10:20:35 +0300

    Add files via upload

--- index.php
+++ index.php
@@ -1,4 +1,148 @@
-<?php
-echo '<h1> Hello World </h1>';
-?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title><?= 'My Site!' ?></title>
+    <!-- Meta -->
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <!-- Global CSS -->
+    <link rel="stylesheet" href="assets/my-styles.css">
 
+
+</head>
+<body>
+<header id="header" class="header">
+    <div class="container">
+        <p class="story">Harry Potter</p>
+        <div class="top-bar">
+            <div class="block">
+                <p class="marcy">Magic..</p>
+            </div>
+        </div>
+    </div>
+
+    <div class="nav-menu">
+        <ul class="menu">
+            <li class="link">Home Page</li>
+            <li class="link">About Us</li>
+            <li class="link">Contact Us</li>
+        </ul>
+    </div>
+    </div>
+</header>
+
+
+<!---------------------------------------------------------------------------------------------------->
+
+
+<main class="main content">
+    <div class="row custom_new">
+        <div class="div-text">
+            <h3 class="title">Who is a Garry Potter?</h3>
+            <p class="text">
+                «Гаррі Поттер» (англ. Harry Potter) — серія з семи фантастичних романів англійської письменниці Джоан Роулінг.
+                У книгах розповідається про пригоди юного чарівника Гаррі Поттера і його друзів Герміону Грейнджер і Рона Візлі, які навчаються
+                у Гоґвортській школі чарів і чаклунства. Основний сюжет присвячений боротьбі Гаррі проти темного чаклуна лорда Волдеморта,
+                який намірюється стати безсмертним, повалити чаклунський керівний орган званий Міністерством магії і підкорити всіх чарівників і маґлів (не чарівників).
+            </p>
+            <p class="text">
+                Проєкт «Гаррі Поттер» став одним з найуспішніших літературних проєктів.
+                Ніколи раніше книги, тим більше дитячі, не видавалися таким тиражем. Перші 6 книг видалися тиражем 325 мільйонів екземплярів,
+                перекладені на 64 мови. При цьому слід врахувати, що більша частина населення планети
+                розмовляє десятьма мовами: англійською, російською, іспанською, французькою, німецькою, арабською, турецькою, китайською, японською, хінді.
+                <!----------------------------------------------------------------------------------------->
+
+            </p>
+            <p class="link">
+                <a href="article.html" class="full-story">Full Story</a>
+            </p>
+        </div>
+        <div class="image-block">
+            <img src="img/hogwarts.jpg" alt="Right Image" class="image">
+        </div>
+        <div class="img-absolute">
+            <img src="img/garry.jpg" alt="Image on Top" class="img-above">
+        </div>
+    </div>
+
+    <!------------------------------------------------------------------------------------------>
+
+    <div class="row">
+        <div class="div-quotation div-text">
+            <h3 class="title">Це в тебе емоційний діапазон як у зубочистки !
+                </h3>
+            <div class="image-block">
+                <img src="img/Germiona.jpg" alt="Centre Image" class="image">
+            </div>
+            <p class="text">
+                Ставлення до серії книг неоднозначне: з одного боку — це захоплива казкова історія,
+                яка дає можливість поринути у світ магії, але з іншого — структурований засіб впливу на дитячий світогляд.
+                У світі чарівників присутнє як позитивне, так і різко негативне ставлення до маґлів (звичайних людей).
+                Дехто вважає їх друзями, а дехто ставиться вороже. Навіть на батьківщині письменниці.
+            </p>
+            <p class="text">
+                Так, генеральний секретар Союзу педагогів та університетських професорів Англії Пітер Сміт попереджував,
+                що «прем'єра фільму про Гаррі Поттера призведе (не багато не мало) до появи нового покоління, яке відкрило для себе світ магії».
+                В 1999 році в США книги про Гаррі Поттера намагалися заборонити 23 рази в 13 штатах. Не зважаючи на це книги про юного чарівника ввійшли до класики сучасної дитячої літератури.
+                іУ багатьох країнах книги про Гаррі Поттера стали частиною шкільної програми з літератури.
+            </p>
+            <p class="link">
+                <a href="article.php" class="full-story">Full Story</a>
+            </p>
+        </div>
+    </div>
+</main>
+
+<!--------------------------------------------------------------------------------------------------------->
+<div class="row iframe">
+    <iframe width="100%" height="480"
+            src=https://youtu.be/g3z-VJRLzvsframeborder="0"
+            allowfullscreen></iframe>
+</div>
+<!----------------youtube------------->
+
+<!-------------------------------------------------------------------------------------------------------->
+
+
+
+<section class="container_block">
+    <div class="row">
+        <div class="after-iframe">
+            <div class="image-block">
+                <img src="img/dobby.jpg" alt="Left Image" class="image1">
+                <div class="text">
+                    <h2>Додатково</h2>
+                    <p>Відзняті усі сім частин книги. Останню частину розбито на два фільми з приставками "Частина 1" та "Частина 2" у назві. </p>
+                </div>
+                <div class="f_text">
+                    <p>“У Гоґвортсі той хто просить допомоги ,Гаррі, завжди її отримує ”</p>
+                    <div class="author">
+                        <p> </p><span></span>
+                    </div>
+                    <a href="article.php" class="more">Full Story</a>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+
+<!------------------------------------------------------------------------------------------------>
+
+
+<div class="nav-menu">
+    <div id="burger">
+        <span></span><span></span><span></span>
+    </div>
+    <ul class="menu">
+        <li><a href="/">Home Page</a></li>
+        <li><a href="/about.php">About Us</a></li>
+        <li><a href="/login.php">Log in</a></li>
+    </ul>
+</div>
+
+
+</body>
+</html>
